ffc2fc50b2a547061a9ee678115483347ec261a6
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-03-17 07:44:40 -0300

    initial commit

---
+++ src/main/java/br/com/mystore/desktop/MystoreDesktopApplication.java
@@ -0,0 +1,13 @@
+package br.com.mystore.desktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystoreDesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystoreDesktopApplication.class, args);
+	}
+
+}
